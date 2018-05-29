a995823c521db6215b8649a995782c08c5e8e8c5
Michael MacLeod <michaelmmacleod@gmail.com>
2018-05-29 13:54:34 -0700

    Remoe a function that can be replaced with ``$''

--- haskell/mandelbrot.hs
+++ haskell/mandelbrot.hs
@@ -1,15 +1,13 @@
 import Data.Complex
 import Data.List
 
-(<|) a b = a b
-
-main = putStrLn <| intercalate "\n" grid
+main = putStrLn $ intercalate "\n" grid
 
 mandelbrot z m n c 
     | magnitude z >= 2 || n == m = n
     | otherwise = mandelbrot (z * z + c) m (n + 1) c
 
-grid = [[toChar <| escapeTime (a :+ 2 * bi) | a <- bounds_a] | bi <- bounds_bi]
+grid = [[toChar $ escapeTime (a :+ 2 * bi) | a <- bounds_a] | bi <- bounds_bi]
     where
         escapeTime = mandelbrot 0 1000 0
         toChar c   = "$@B%8&WM#*|1?-_+~i!I:,'. " !! (mod c 25)
